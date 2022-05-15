b47249b9c429bad687a810bb5972fb0deacc49af
Kristine Johnson <kristinethejohnson@gmail.com>
2022-05-15 02:34:19 -0400

    form section completed, hand animation

--- index.js
+++ index.js
@@ -169,9 +169,8 @@ for (const link of filterLink) {
 //   }
 // };
 
+//Modal
 const open = "open";
-// const active = "active";
-
 const modalOpen = "[data-open]";
 const modalClose = "[data-close]";
 const isVisible = "is-visible";
@@ -192,8 +191,6 @@ for (const elm of closeModal) {
   });
 }
 
-// Modal
-
 document.addEventListener("click", (e) => {
   if (e.target === document.querySelector(".modal.is-visible")) {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
@@ -204,4 +201,159 @@ document.addEventListener("keyup", (e) => {
   if (e.key === "Escape") {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
+});
+
+//Contact Form
+
+const containers = document.querySelectorAll(".input-container");
+const form = document.querySelector("form");
+
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+
+//Line
+const start =
+  "M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
+const end =
+  "M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512";
+
+//Elastic
+
+containers.forEach((container) => {
+  const input = container.querySelector(".input");
+  const line = container.querySelector(".elastic-line");
+  const placeholder = container.querySelector(".placeholder");
+  input.addEventListener("focus", () => {
+    if (!input.value) {
+      timeline.fromTo(
+        line,
+        { attr: { d: start } },
+        { attr: { d: end }, ease: "Power2.easeOut", duration: 0.75 }
+      );
+      timeline.to(
+        line,
+        { attr: { d: start }, ease: "elastic.out(3, 0.5)" },
+        "<50%"
+      );
+      timeline.to(
+        placeholder,
+        {
+          top: -15,
+          left: 0,
+          scale: 0.7,
+          duration: 0.5,
+          ease: "Power2.easeOut",
+        },
+        "<15%"
+      );
+    }
+  });
+});
+
+form.addEventListener("click", () => {
+  containers.forEach((container) => {
+    const input = container.querySelector(".input");
+    const line = container.querySelector(".elastic-line");
+    const placeholder = container.querySelector(".placeholder");
+
+    if (document.activeElement !== input) {
+      if (!input.value) {
+        gsap.to(placeholder, {
+          top: 0,
+          left: 0,
+          scale: 1,
+          duration: 0.5,
+          ease: "Power2.easeOut",
+        });
+      }
+    }
+    input.addEventListener("input", (e) => {
+      if (e.target.type === "text") {
+        let inputText = e.target.value;
+        if (inputText.length > 2) {
+          colorize("#C3D0C1", line, placeholder);
+        } else {
+          colorize("#DDAF9D", line, placeholder);
+        }
+      }
+      if (e.target.type === "email") {
+        let valid = validateEmail(e.target.value);
+        if (valid) {
+          colorize("#C3D0C1", line, placeholder);
+        } else {
+          colorize("#DDAF9D", line, placeholder);
+        }
+      }
+      if (e.target.type === "tel") {
+        let valid = validatePhone(e.target.value);
+        if (valid) {
+          colorize("#C3D0C1", line, placeholder);
+        } else {
+          colorize("#DDAF9D", line, placeholder);
+        }
+      }
+    });
+  });
+});
+
+// checking email validation
+
+function validateEmail(email) {
+  let re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+function validatePhone(phone) {
+  let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(phone);
+}
+
+function colorize(color, line, placeholder) {
+  gsap.to(line, { stroke: color, duration: 0.75 });
+  gsap.to(placeholder, { color: color, duration: 0.75 });
+}
+
+//Animating Character
+gsap.set("#eye", { transformOrigin: "center" });
+gsap.fromTo(
+  "#eye",
+  { scaleY: 1 },
+  {
+    scaleY: 0.3,
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 0.5,
+    ease: "Power2.easeOut",
+  }
+);
+
+gsap.fromTo(
+  "#eyebrow",
+  { y: 0 },
+  { y: -1, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: "Power2.easeOut" }
+);
+
+const submitButton = document.querySelector(".contact-sbt-btn");
+const timelineThree = gsap.timeline({
+  defaults: { duration: 0.75, ease: "Power2.easeOut" },
+});
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  timelineThree.to(".contact-right, .contact-left", {
+    y: 30,
+    opacity: 0,
+    pointerEvents: "none",
+  });
+  timelineThree.to("form", { scale: 0.8 }, "<");
+  timelineThree.fromTo(
+    ".submitted",
+    { opacity: 0, y: 30 },
+    { opacity: 1, y: 0 }
+  );
+  //Hand wave
+  gsap.set("#hand", { transformOrigin: "left" });
+  gsap.fromTo(
+    "#hand",
+    { rotation: 0, y: 0 },
+    { rotation: -10, y: 2, ease: "elastic(3,0.3)", duration: 2, delay: 1 }
+  );
 });
