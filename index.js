94c36dae8daed717e7924b13aa1fa461bcf7c8ab
Kristine Johnson <100108249+kristinejohnson7@users.noreply.github.com>
2022-05-31 22:09:12 -0400

    update to nav mobile responsiveness, add project for debt calc

--- index.js
+++ index.js
@@ -62,11 +62,15 @@ new simpleParallax(image, {
 
 //Nav Events
 
+const navLogo = document.querySelector(".nav-logo");
+
 window.onscroll = function () {
   if (window.pageYOffset > 100) {
     nav.classList.add("sticky");
+    navLogo.classList.add("sticky");
   } else {
     nav.classList.remove("sticky");
+    navLogo.classList.remove("sticky");
   }
 };
 
@@ -190,13 +194,10 @@ const form = document.querySelector("form");
 
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
 
-//Line
 const start =
   "M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
 const end =
   "M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512";
-
-//Elastic
 
 containers.forEach((container) => {
   const input = container.querySelector(".input");
